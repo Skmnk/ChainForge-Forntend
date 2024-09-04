b693d50de1b24e911f62d66d908a36a21880cab6
Skmnk <dreamsrikrishna7@gmail.com>
2024-09-04 18:47:07 +0530

    Add localhost

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'https://chainforge-backend.onrender.com/';
+const API_URL = 'http://localhost:5000/api/auth';
 
 // Register User
 export const registerUser = async (name, email, password, confirmPassword) => {
     try {
-        const response = await axios.post(`https://chainforge-backend.onrender.com/register`, {
+        const response = await axios.post(`http://localhost:5000/api/auth/register`, {
             name,
             email,
             password,
@@ -22,7 +22,7 @@ export const registerUser = async (name, email, password, confirmPassword) => {
 export const loginUser = async (email, password) => {
     console.log(`trying to try ${email}`)
     try {
-        const response = await axios.post(`https://chainforge-backend.onrender.com/login`, {
+        const response = await axios.post(`http://localhost:5000/api/auth/login`, {
             email,
             password,
         });
